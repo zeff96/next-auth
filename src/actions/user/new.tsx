823ffc22826b6d472001d185bc08650cf20d6b06
zeff96 <zeffo1975@gmail.com>
2023-11-14 19:44:22 +0300

    Parse user object schema

--- src/actions/user/new.tsx
+++ src/actions/user/new.tsx
@@ -5,11 +5,19 @@ import bcrypt from 'bcrypt'
 import { z } from "zod"
 
 export const registration = async(prevState: any, formData: FormData) => {
-  
+
   const schema = z.object({
     username: z.string(),
     email: z.string(),
     password: z.string(),
     passwordConfirmation: z.string(),
   })
+
+  const user = schema.parse({
+    username: formData.get('username'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+    passwordConfirmation: formData.get('password_confirmation')
+  })
+  
 }
