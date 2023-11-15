02ee54dfed0d5f46f444add1225fbf75457117b2
zeff96 <zeffo1975@gmail.com>
2023-11-15 20:25:07 +0300

    Add user data into cookies

--- src/actions/user/login.tsx
+++ src/actions/user/login.tsx
@@ -1,13 +1,14 @@
 import bcrypt from 'bcrypt'
-import {string, z} from 'zod'
+import {z} from 'zod'
 import jwt from "jsonwebtoken"
+import {cookies} from 'next/headers'
 import { pool } from '@/db/db'
 
 const loginUser = async(prevState: any, formData: FormData) => {
 
   const schema = z.object({
-    email: string(),
-    password: string()
+    email: z.string(),
+    password: z.string()
   })
 
   const user = schema.parse({
@@ -28,4 +29,12 @@ const loginUser = async(prevState: any, formData: FormData) => {
   const result = await pool.query('SELECT id, username, email FROM users WHERE email = $1', [user.email])
 
   const token = jwt.sign({id: result.rows[0].id, username: result.rows[0].username, email: result.rows[0].email}, 'secret', {expiresIn: "1hr"})
+
+  cookies().set({
+    name: 'user',
+    value: token,
+    httpOnly: true,
+    secure: true,
+    expires: Date.now() + 24 * 60 * 60 * 1000
+  })
 }
