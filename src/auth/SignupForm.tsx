a15ccf47e3dcb6422596c42311a1a10e8ae181a3
zeff96 <zeffo1975@gmail.com>
2023-11-16 21:37:05 +0300

    Add authentication form

--- src/auth/SignupForm.tsx
+++ src/auth/SignupForm.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React, {useState} from "react"
 import {useFormState, useFormStatus} from "react-dom"
 import { registration } from "@/actions/user/new"
 
@@ -14,5 +15,35 @@ const SubmitButton = () => {
     <button type="submit" aria-disabled={pending}>
       Sign up
     </button>
+  )
+}
+
+const SignupForm = () => {
+  const[state, formAction] = useFormState(registration, initialState)
+  const[show, setShow] = useState(false)
+
+  return(
+    <form action={formAction}>
+      <label htmlFor="username">
+        Username
+        <input type="text" name="username" id="username" placeholder="Enter name" required />
+      </label>
+      <label htmlFor="email">
+        Email
+        <input type="email" name="email" id="email" placeholder="Enter email" required />
+      </label>
+      <label htmlFor="password">
+        Password
+        <input type="password" name="password" id="password" placeholder="Enter password" required />
+      </label>
+      <label htmlFor="password_confirmation">
+        Password confirmation
+        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm password" required />
+      </label>
+      <SubmitButton />
+      <p aria-live="polite" role="status">
+        {state?.message}
+      </p>
+    </form>
   )
 }
