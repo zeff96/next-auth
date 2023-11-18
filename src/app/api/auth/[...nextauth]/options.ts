d448016b3d5f50b0d85cb6ef2df610c40a06a81a
zeff96 <zeffo1975@gmail.com>
2023-11-18 23:48:15 +0300

    Add credentials provider to providers array

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,26 @@
 import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { pool } from "@/db/db";
 
 export const options: NextAuthOptions = {
-  providers: []
+  providers: [
+    CredentialsProvider({
+      name: 'Sign in with email and password',
+      credentials: {
+        email: {label: 'Email', type: 'email', placeholder: 'Email'},
+        password: {label: 'Password', type: 'password', placeholder: "Password"}
+      },
+      async authorize(credentials, req){
+        const result = await pool.query('SELECT id, username, email FROM users WHERE email = $1', [credentials?.email])
+
+        const user = result.rows[0]
+
+        if(user) {
+          return user
+        }else {
+          return null
+        }
+      }
+    })
+  ]
 }
