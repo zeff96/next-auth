2da9e738c3c27ab048b72ed23420e698a907a46c
zeff96 <zeffo1975@gmail.com>
2023-11-25 20:32:49 +0300

    Add try catch block

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -10,15 +10,19 @@ interface requestBody {
 export async function POST(request: Request) {
   const body: requestBody = await request.json()
 
-  const user = await prisma.user.create({
-    data: {
-      name: body.name,
-      email: body?.email,
-      password: await bcrypt.hash(body?.password, 10)
-    }
-  })
-
-  const { password, ...result } = user
-
-  return new Response(JSON.stringify(result))
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body?.email,
+        password: await bcrypt.hash(body?.password, 10)
+      }
+    })
+  
+    const { password, ...result } = user
+  
+    return new Response(JSON.stringify(result))
+  } catch (error) {
+    return new Response("Error creating user", {status: 500})
+  }
 }
