5d1abec8db637f98d818edabde2e565d074fff57
zeff96 <zeffo1975@gmail.com>
2023-11-28 00:47:15 +0300

    Add prisma adapter to persist user

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,12 @@
 import type { NextAuthOptions } from "next-auth";
+import {PrismaAdapter} from '@auth/prisma-adapter'
 import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
+import prisma from "@/lib/prisma";
 
 export const options: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
